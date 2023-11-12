702b03f6c28e2434efb1d000b2a37d88b3072368
Roman <roman-right@protonmail.com>
2023-11-11 21:56:27 -0600

    classes in the __init__.py

--- beanie_batteries_queue/__init__.py
+++ beanie_batteries_queue/__init__.py
@@ -1,8 +1,13 @@
+from beanie_batteries_queue.queue import Queue
+from beanie_batteries_queue.runner import Runner
 from beanie_batteries_queue.task import Task, State, Priority, DependencyType
-
+from beanie_batteries_queue.worker import Worker
 
 __all__ = [
     "Task",
+    "Queue",
+    "Worker",
+    "Runner",
     "State",
     "Priority",
     "DependencyType",
